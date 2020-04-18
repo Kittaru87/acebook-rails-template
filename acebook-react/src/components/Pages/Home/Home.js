025e6bb50e68f57c2d741104f0b5a9e385a406a1
nimzyow <n_soufiani@hotmail.com>
2020-04-18 20:17:31 +0100

    removed buttons from home

--- acebook-react/src/components/Pages/Home/Home.js
+++ acebook-react/src/components/Pages/Home/Home.js
@@ -2,17 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div>
-      Hello from home
-      <div>
-        <Link to="/profile">To Profile page from Link</Link>
-        <button>
-          <Link to="/profile">To Profile page from button with Link</Link>
-        </button>
-      </div>
-    </div>
-  );
+  return <div>Hello from home</div>;
 };
 
 export default Home;
